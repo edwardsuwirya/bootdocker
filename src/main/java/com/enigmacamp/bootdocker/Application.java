b4f76daddb4f1fbdaa995b3bf750090aacac589d
edwardsuwirya <edward.suwirya@ffi.co.id>
2019-12-12 16:05:23 +0700

    10th init

--- src/main/java/com/enigmacamp/bootdocker/Application.java
+++ src/main/java/com/enigmacamp/bootdocker/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Coba donggg...";
+        return "Hiiii helen";
     }
 
     public static void main(String[] args) {
